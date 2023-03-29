7c3caa83f2165f948cb4860d51560b244e6fd474
Anghelus Andrei <anghelusandrei2@gmail.com>
2023-03-29 20:37:26 +0300

    v0.1

---
+++ build/CMakeFiles/Voxel_Renderer.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "C:/Users/Admin/Desktop/Voxel_Renderer/bin/Debug/Voxel_Renderer.exe"
+  "C:/Users/Admin/Desktop/Voxel_Renderer/bin/Debug/Voxel_Renderer.exe.manifest"
+  "C:/Users/Admin/Desktop/Voxel_Renderer/bin/Debug/Voxel_Renderer.pdb"
+  "C:/Users/Admin/Desktop/Voxel_Renderer/bin/Debug/libVoxel_Renderer.dll.a"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_demo.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_demo.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_draw.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_draw.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_impl_sdl2.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_impl_sdl2.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_impl_sdlrenderer.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_impl_sdlrenderer.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_tables.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_tables.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_widgets.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/imgui/internal/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/Voxel_Renderer.dir/src/main.cpp.obj"
+  "CMakeFiles/Voxel_Renderer.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Voxel_Renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Voxel_Renderer.dir/src/main.cpp.obj.d
@@ -0,0 +1,75 @@
+CMakeFiles/Voxel_Renderer.dir/src/main.cpp.obj: \
+ C:\Users\Admin\Desktop\Voxel_Renderer\src\main.cpp \
+ C:\Users\Admin\Desktop\Voxel_Renderer\src\imgui/internal/imgui.h \
+ C:\Users\Admin\Desktop\Voxel_Renderer\src\imgui/internal/imconfig.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\float.h c:\mingw\include\float.h \
+ c:\mingw\include\_mingw.h c:\mingw\include\msvcrtver.h \
+ c:\mingw\include\w32api.h c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ c:\mingw\include\string.h c:\mingw\include\strings.h \
+ c:\mingw\include\wchar.h c:\mingw\include\assert.h \
+ C:\Users\Admin\Desktop\Voxel_Renderer\src\imgui/internal/imgui_impl_sdl2.h \
+ C:\Users\Admin\Desktop\Voxel_Renderer\src\imgui/internal/imgui_impl_sdlrenderer.h \
+ c:\mingw\include\stdio.h c:\mingw\include\sys/types.h C:/include/SDL.h \
+ C:/include/SDL_main.h C:/include/SDL_stdinc.h C:/include/SDL_config.h \
+ C:/include/SDL_platform.h C:/include/begin_code.h \
+ C:/include/close_code.h c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
+ c:\mingw\include\stdint.h C:/include/SDL_assert.h \
+ C:/include/SDL_atomic.h C:/include/SDL_audio.h C:/include/SDL_error.h \
+ C:/include/SDL_endian.h C:/include/SDL_mutex.h C:/include/SDL_thread.h \
+ c:\mingw\include\process.h c:\mingw\include\sys\types.h \
+ c:\mingw\include\stdint.h C:/include/SDL_rwops.h \
+ C:/include/SDL_clipboard.h C:/include/SDL_cpuinfo.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\immintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\mmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\xmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\mm_malloc.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\stdlib.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\cstdlib \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\c++config.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\os_defines.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\c++\mingw32\bits\cpu_defines.h \
+ c:\mingw\include\stdlib.h c:\mingw\include\errno.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\emmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\pmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\tmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\smmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\popcntintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\wmmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avxintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx2intrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512fintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512erintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512pfintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512cdintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512bwintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512dqintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vlbwintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vldqintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512ifmaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512ifmavlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vbmiintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\avx512vbmivlintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\shaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\lzcntintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\bmiintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\bmi2intrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\fmaintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\f16cintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\rtmintrin.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\xtestintrin.h \
+ C:/include/SDL_events.h C:/include/SDL_video.h C:/include/SDL_pixels.h \
+ C:/include/SDL_rect.h C:/include/SDL_surface.h \
+ C:/include/SDL_blendmode.h C:/include/SDL_keyboard.h \
+ C:/include/SDL_keycode.h C:/include/SDL_scancode.h \
+ C:/include/SDL_mouse.h C:/include/SDL_joystick.h C:/include/SDL_guid.h \
+ C:/include/SDL_gamecontroller.h C:/include/SDL_sensor.h \
+ C:/include/SDL_quit.h C:/include/SDL_gesture.h C:/include/SDL_touch.h \
+ C:/include/SDL_filesystem.h C:/include/SDL_haptic.h \
+ C:/include/SDL_hidapi.h C:/include/SDL_hints.h C:/include/SDL_loadso.h \
+ C:/include/SDL_log.h C:/include/SDL_messagebox.h C:/include/SDL_metal.h \
+ C:/include/SDL_power.h C:/include/SDL_render.h C:/include/SDL_shape.h \
+ C:/include/SDL_system.h C:/include/SDL_timer.h C:/include/SDL_version.h \
+ C:/include/SDL_locale.h C:/include/SDL_misc.h
